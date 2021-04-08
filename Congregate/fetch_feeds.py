41d44c4e678e3746403b8b8f408151b11b85859d
PreetThakkar <preetthakkar85@gmail.com>
2021-04-08 21:07:49 +0530

    fetch_feed stores content to csv faster than views stores to db.

--- Congregate/fetch_feeds.py
+++ Congregate/fetch_feeds.py
@@ -1,8 +1,18 @@
 from feeds import *
+import csv
 import time
 start_time = time.time()
 objects = [bbc.BBC(), engadget.Engadget(), medium.Medium(), byteiota.Byteiota()]
+f = open('data.csv', 'w', encoding='utf-8', newline='')
+writer = csv.writer(f)
+writer.writerow(["Source", "Feed / Category", "Title", "Url"])
 for object in objects:
 	for result in object.getAll():
-		pass
+		try:
+			writer.writerow(result)
+		except UnicodeError as e:
+			print(e)
+			for i in result:
+				print(type(i), i)
 print("--- %s seconds ---" % (time.time() - start_time))
+f.close()
--- Congregate/views.py
+++ Congregate/views.py
@@ -5,17 +5,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	print("Home")
+	with open("data.csv", 'rb') as f:
+		for line in f.readlines():
+			print(line.decode())
 
 def store(request):
 	objects = [bbc.BBC(), engadget.Engadget(), medium.Medium(), byteiota.Byteiota()]
 	for object in objects:
-		for results in object.getAll():
-			for result in results:
-				mod = Content_Model()
-				mod.source = result[0]
-				mod.category = result[1]
-				mod.title = result[2]
-				mod.url = result[3]
-				mod.save()
+		for result in object.getAll():
+			mod = Content_Model()
+			mod.source = result[0]
+			mod.category = result[1]
+			mod.title = result[2]
+			mod.url = result[3]
+			mod.save()
 	return HttpResponse('<h1>Success Storage of Content</h1>')
