e20c0416d3c3113833fef28db299ee15d6d7b1cd
PreetThakkar <preetthakkar85@gmail.com>
2021-04-26 16:05:13 +0530

    User Interface imporved.
    Limited home screen content to 5.
    There is dedicated viewing page for each source.
    To-do: Eliminate some repetitive code.

--- Congregate/urls.py
+++ Congregate/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name = "home"),
+    path('source/<str:s>/#<str:c>', views.home_cat, name = "particular"),
+    path('source/<str:s>', views.home_all, name = "main"),
     # path('store/', views.store, name = "store"),
 ]
--- Congregate/views.py
+++ Congregate/views.py
@@ -14,19 +14,73 @@ def home(request):
 	# storee.delay(0)
 	data = []
 	sources = {}
+	cat_count = defaultdict(int)
 	try:
 		f = open("data_new.csv", 'r', encoding='utf-8')
 	except Exception as e:
 		f = open("data_old.csv", 'r', encoding='utf-8')
 	reader = csv.reader(f)
 	for row in reader:
-		data.append(row)
-		if row[0] not in sources.keys():
-			sources[row[0]] = []
-		if row[1] not in sources[row[0]]:
-			sources[row[0]].append(row[1])
+		current = " - ".join([row[0], row[1]])
+		if cat_count[current] == 0:
+			if row[0] not in sources.keys():
+				sources[row[0]] = []
+			if row[1] not in sources[row[0]]:
+				sources[row[0]].append(row[1])
+		if cat_count[current] < 5:
+			data.append(row)
+			cat_count[current] += 1
 	name = f.name
 	f.close()
 	if name == "data_new.csv":
 		shutil.move(name, "data_old.csv")
-	return render(request, 'index.html', context = {'data': data, 'sources': sources})
+	return render(request, 'flex-index.html', context = {'data': data, 'sources': sources, 'rang': range(5)})
+
+
+def home_all(request, s):
+	# fetch feeds from data_new.py and then rename it to data_old.csv. if not then fetch from data.old.csv.
+	# storee.delay(0)
+	data = []
+	sources = {}
+	# cat_count = defaultdict(int)
+	try:
+		f = open("data_new.csv", 'r', encoding='utf-8')
+	except Exception as e:
+		f = open("data_old.csv", 'r', encoding='utf-8')
+	reader = csv.reader(f)
+	for row in reader:
+		if row[0] == s:
+			data.append(row)
+			if row[0] not in sources.keys():
+				sources[row[0]] = []
+			if row[1] not in sources[row[0]]:
+				sources[row[0]].append(row[1])
+	name = f.name
+	f.close()
+	if name == "data_new.csv":
+		shutil.move(name, "data_old.csv")
+	return render(request, 'index.html', context = {'data': data, 'sources': sources, 'rang': range(5)})
+
+def home_cat(request, s, c=''):
+	# fetch feeds from data_new.py and then rename it to data_old.csv. if not then fetch from data.old.csv.
+	# storee.delay(0)
+	data = []
+	sources = {}
+	# cat_count = defaultdict(int)
+	try:
+		f = open("data_new.csv", 'r', encoding='utf-8')
+	except Exception as e:
+		f = open("data_old.csv", 'r', encoding='utf-8')
+	reader = csv.reader(f)
+	for row in reader:
+		if row[0] == s:
+			data.append(row)
+			if row[0] not in sources.keys():
+				sources[row[0]] = []
+			if row[1] not in sources[row[0]]:
+				sources[row[0]].append(row[1])
+	name = f.name
+	f.close()
+	if name == "data_new.csv":
+		shutil.move(name, "data_old.csv")
+	return render(request, 'index.html', context = {'data': data, 'sources': sources, 'rang': range(5)})
